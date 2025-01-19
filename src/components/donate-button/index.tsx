59c69d0bfb907342c6af29e6a19724e8f7962371
Bruno Correia <bruninhocorreia2013@gmail.com>
2025-01-18 22:17:16 -0300

    refactor: new donate button

--- src/components/donate-button/index.tsx
+++ src/components/donate-button/index.tsx
@@ -1,20 +1,27 @@
 import { CiCoffeeCup } from "react-icons/ci";
 import Link from "next/link";
 
+type DonateButtonVariant = "default" | "small";
+
 type DonateButtonProps = {
   donateUrl: string;
-  message: string;
+  message?: string;
+  variant?: DonateButtonVariant;
 };
 
 export default function DonateButton(props: DonateButtonProps) {
+  const { donateUrl, message, variant = "default" } = props;
+
+  const shouldShowMessage = variant === "default" && message !== undefined;
+
   return (
     <Link
       target="_blank"
-      className="inline-flex items-center text-black bg-yellow-400 border border-transparent rounded-lg py-2 px-4 text-lg tracking-wide font-cursive shadow-md hover:underline hover:opacity-85 focus:opacity-85 focus:ring-2 focus:ring-yellow-400"
-      href={props.donateUrl}
+      className="inline-flex items-center text-black bg-yellow-400 border border-transparent rounded-lg py-2 px-2 text-lg tracking-wide font-cursive shadow-md hover:scale-105 transition-all duration-200 ease-in-out gap-2"
+      href={donateUrl}
     >
-      <CiCoffeeCup className="w-6 h-6 text-black" />
-      <span className="ml-4 text-sm">{props.message}</span>
+      <CiCoffeeCup size={20} className="text-black" />
+      {shouldShowMessage && <span className="text-sm">{message}</span>}
     </Link>
   );
 }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,16 @@
 import { FaBusAlt } from "react-icons/fa";
+import DonateButton from "../donate-button";
 
-export default function Header(){
+export default function Header() {
   return (
-    <header className="bg-black h-16 py-5 flex items-center justify-center">
-      <FaBusAlt className="w-8 h-8 text-white text-2xl" />
+    <header className="bg-black h-16 px-3 py-2 flex items-center justify-center">
+      <div className="flex flex-1 items-center justify-between w-full h-full max-w-7xl sm:px-6 lg:px-8">
+        <FaBusAlt className="w-8 h-8 text-white text-2xl" />
+        <DonateButton
+          donateUrl="https://buymeacoffee.com/buzufba"
+          variant="small"
+        />
+      </div>
     </header>
   );
-};
-
+}
