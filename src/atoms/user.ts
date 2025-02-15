a6fdc27c18617f89565d65e75241454c422b9e43
Bruno Correia <74073643+brnocorreia@users.noreply.github.com>
2025-02-15 11:27:29 -0300

    Revert "feat: add mocked user flow (#3)" (#4)
    This reverts commit 2b5c7692472c359cebc951347870b4b3bbf3ca50.

--- src/atoms/user.ts
+++
@@ -1,8 +0,0 @@
-import { atomWithStorage } from "jotai/utils";
-import { LocalStorageKeys } from "@/constants/local-storage-keys";
-import { User } from "@/@types/user";
-
-export const userAtom = atomWithStorage<User | null>(
-  LocalStorageKeys.USER,
-  null
-);
