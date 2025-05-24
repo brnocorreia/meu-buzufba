7591829ab92f7b1b32f06b023a61a29c0371e7fd
Bruno Correia <bruninhocorreia2013@gmail.com>
2025-05-24 18:27:50 -0300

    chore: minor adjust in padding

--- src/components/next-departure-route-line/index.tsx
+++ src/components/next-departure-route-line/index.tsx
@@ -26,7 +26,7 @@ export const NextDepartureRouteLine = ({
   }, [departureTime]);
 
   return (
-    <div className="flex flex-row items-center gap-2 w-full bg-zinc-800 rounded-md p-2">
+    <div className="flex flex-row items-center gap-2 w-full bg-zinc-800 rounded-md px-3 py-1">
       <div className="flex flex-row items-center gap-2 w-full flex-1">
         <div className="flex flex-row items-center justify-center w-14 h-14 bg-zinc-600 rounded-md">
           <Bus size={28} className="text-zinc-300" />
